ffdf3fe58972a4ab7605211ef5b187577d20d873
kimseungdeok <tmdejr1117@gmail.com>
2022-12-30 23:42:12 +0900

    fix : npm run dev 안되는것 해결(ws 설치)

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import http from "http";
+import WebSocket from "ws";
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handelListen = () => console.log(`Listening on http://localhost:3000`);
+
+const server = http.createServer(app);// http 서버 생성
+
+const wss = new WebSocket.Server({ server });// 웹소켓 서버 생성
+
+server.listen(3000, handelListen);
